50967096e37b638c05dc70548d32c8a2dbeb72a6
aashnasahu24 <155808125+aashnasahu24@users.noreply.github.com>
2024-01-31 16:06:21 +0530

    Add files via upload

--- codes/1.py
+++ codes/1.py
@@ -6,26 +6,20 @@ from matplotlib.ticker import MultipleLocator
 with open('1.dat', 'r') as file:
     lines = file.readlines()
 
-# Extract data from each line
-x_values = []
-y1_values = []
-y2_values = []
+# Extract data from each line using list comprehension
+data = [list(map(float, line.split())) for line in lines]
 
-for line in lines:
-    values = line.split()
-    x_values.append(float(values[0]))
-    y1_values.append(float(values[1]))
-    y2_values.append(float(values[2]))
-    
-# Plot the data
+# Unpack data using numpy array operations
+x_values, y1_values, y2_values = np.array(data).T
+
+# Plot the data for the first GP
 plt.figure(figsize=(8, 8))
 
-# Plot for the first GP
 plt.plot(x_values, y1_values, 'ro-', markersize=0, linewidth=0.3)
 plt.stem(x_values, y1_values, basefmt='b-', linefmt='-', markerfmt='ro')
 plt.title('General Term of Geometric Progression')
 plt.xlabel('n')
-plt.ylabel('Term Value')
+plt.ylabel('x_1(n)')
 
 plt.gca().xaxis.set_major_locator(MultipleLocator(1))
 
@@ -33,16 +27,18 @@ plt.grid(True)
 plt.savefig('../figs/plot1.png')
 plt.show()
 
-# Plot for the second GP
+# Plot the data for the second GP
+plt.figure(figsize=(8, 8))
+
 plt.plot(x_values, y2_values, 'ro-', markersize=0, linewidth=0.3)
 plt.stem(x_values, y2_values, basefmt='b-', linefmt='-', markerfmt='ro')
 plt.title('General Term of Geometric Progression')
 plt.xlabel('n')
-plt.ylabel('Term Value')
+plt.ylabel('x_2(n)')
 
 plt.gca().xaxis.set_major_locator(MultipleLocator(1))
 
 plt.grid(True)
 plt.savefig('../figs/plot2.png')
-
 plt.show()
+
